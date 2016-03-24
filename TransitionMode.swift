6b65c1dc639bd368ab6f25e2fc57d9c9eb2d2b9d
Adolfo <fitomad@users.noreply.github.com>
2016-03-24 20:10:03 +0100

    Create TransitionMode.swift
    Transition state

---
+++ TransitionMode.swift
@@ -0,0 +1,21 @@
+//
+//  TransitionMode.swift
+//  desappstre framework
+//
+//  Created by Adolfo Vera Blasco on 14/03/16.
+//  Copyright (c) 2016 Adolfo Vera Blasco. All rights reserved.
+//
+
+import Foundation
+
+/**
+    The possible directions of the transition.
+     
+    - Present: For presenting a new controller
+    - Dismiss: For dismissing the current controller
+*/
+public enum TransitionMode
+{
+	case Present
+	case Dismiss
+}
